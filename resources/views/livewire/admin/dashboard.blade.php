6fb2e15f709fdf5471dfeb7ab0fb387ffc3d71f1
Brannan Atkinson <brannanatkinson@gmail.com>
2023-09-07 14:15:17 -0500

    Fixing dash errors

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -17,6 +17,43 @@
         
         <div class="max-w-5xl mx-auto">
             @livewire('results')
+        </div>
+        <div class="mt-8 max-w-5xl mx-auto">
+            <div class="my-3 text-3xl font-bold">
+                @php echo date("Y") @endphp Host Summary
+            </div>
+            <div class="grid grid-cols-3 gap-8 mb-10">
+                <div class="p-8 text-center bg-gray-200 rounded-md flex flex-col justify-center items-center">
+                    <div class="mb-6 uppercase">
+                        Total Raised by Hosts
+                    </div>
+                    <div class="mb-8 text-4xl font-bold">
+                        ${{ App\Models\Gift::where('user_id','!=', null)->where('created_at', '>', '2023-01-01')->sum('gift_total') }}
+                    </div>
+                 </div>
+                 <div class="p-8 text-center bg-gray-200 rounded-md flex flex-col justify-center items-center">
+                    <div class="mb-6 uppercase">
+                        Total Gifts by Hosts
+                    </div>
+                    <div class="mb-8 text-4xl font-bold">
+                        {{ App\Models\Gift::where('user_id','!=', null)->where('created_at', '>', '2023-01-01')->count() }}
+                    </div>
+                 </div>
+                <div><!-- blank  --></div>
+            </div>
+            <div class="mt-8 grid grid-cols-5 gap-4">
+                <div class=" col-span-2 font-bold">Host Name</div>
+                <div class=" font-bold">Amount Raised</div>
+                <div class=" font-bold">Total Gifts</div>
+                <div class=" font-bold">Total Items</div>
+                @foreach( $hosts as $host )
+                <div class=" col-span-2">{{ $host->name }}</div>
+                <div class="">${{ App\Models\Gift::where('user_id', '=', $host->id )->where('created_at', '>', '2023-01-01')->sum('gift_total') }}</div>
+                <div class="">{{ App\Models\Gift::where('user_id', '=', $host->id )->where('created_at', '>', '2023-01-01')->count() }}</div>
+                <div class="">{{ $host->items->sum('pivot.item_quantity') }}</div>
+                @endforeach
+            </div>
+        </div>
             
 
     @elsecan('edit host')
