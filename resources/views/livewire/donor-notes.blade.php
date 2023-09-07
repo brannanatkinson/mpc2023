f1234814b59298316ed75ec74a5d45f64d5a18f9
Brannan Atkinson <brannanatkinson@gmail.com>
2023-09-07 16:59:16 -0500

    donor masonry

--- resources/views/livewire/donor-notes.blade.php
+++ resources/views/livewire/donor-notes.blade.php
@@ -5,18 +5,17 @@
                 @php echo date('Y') @endphp Housing Donor Notes
             </div>
             <p class="text-mp-light-gray text-center text-xl">The Mary Parrish Center residents, alumni, and staff are grateful for your wonderful notes. </p>
-            <div class="mt-9 box-border md:columns-3 gap-8 before:box-inherit after:box-inherit">
+            <div class="mt-9 box-border md:masonry before:box-inherit after:box-inherit">
                 @foreach( $donorsWithNotes as $donor )
                     @if( $donor->first() )
-                        <div class="block">
-                            <div class="flex flex-col items-center px-8 py-6 mb-4 bg-gray-200 rounded-lg break-inside">
-                                <div clsss="mb-8 text-center">
-                                    <i class="fad fa-envelope-open fa-2x text-mp-coral"></i>
-                                </div>
-                                <div class="mt-3">
-                                    <p class="font-display italic leading-loose">{{ $donor->note }}</p>
-                                </div>
+                        <div class="flex flex-col items-center px-8 py-6 mb-4 bg-gray-200 rounded-lg break-inside">
+                            <div clsss="mb-8 text-center">
+                                <i class="fad fa-envelope-open fa-2x text-mp-coral"></i>
                             </div>
+                            <div class="mt-3">
+                                <p class="font-display italic leading-loose">{{ $donor->note }}</p>
+                            </div>
+                            
                         </div>
                     @else
                         <div class="px-8 py-6 my-4 bg-gray-200 rounded-lg break-inside">
