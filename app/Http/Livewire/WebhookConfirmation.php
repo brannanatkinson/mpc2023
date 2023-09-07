4a8306c55ff6d3e4ded558e7cd574ac90d764e98
Brannan Atkinson <brannanatkinson@gmail.com>
2023-09-07 09:03:48 -0500

    Wehbook again

--- app/Http/Livewire/WebhookConfirmation.php
+++ app/Http/Livewire/WebhookConfirmation.php
@@ -34,7 +34,7 @@ class WebhookConfirmation extends Component
         
         // check if event host credited
 
-        if ( Regex::isAlpha($this->result['content']['items'][0]['customFields'][0]['value']) == false ) {
+        if ( Regex::isAlpha($this->result['content']['items'][0]['customFields'][0]['value']) == true ) {
             $userId = User::where('name', $this->result['content']['items'][0]['customFields'][0]['value'])->first()->id;
         } else {
             $userId = null;
