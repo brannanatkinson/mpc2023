a618cd46ec9fceb68221dec680fdfb1879d06510
Brannan Atkinson <brannanatkinson@gmail.com>
2023-09-07 17:54:56 -0500

    ShowHostNames grid updates

--- resources/views/livewire/show-host-names.blade.php
+++ resources/views/livewire/show-host-names.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="grid md:grid-cols-3 lg:grid-col4 px-0 sm:px-6">
+    <div class="max-w-6xl grid md:grid-cols-3 lg:grid-col4 gap-8 px-0 sm:px-6">
         @foreach ( $hosts as $host )
             <div class="text-center font-display text-xl text-white">
                 {{ $host->name }}
