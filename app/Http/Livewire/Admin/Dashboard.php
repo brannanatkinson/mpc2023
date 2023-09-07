7ed9f6b61659d13923429724a9f35ed01b1d4abc
Brannan Atkinson <brannanatkinson@gmail.com>
2023-09-07 13:41:03 -0500

    Updated dashboard to show only 2023

--- app/Http/Livewire/Admin/Dashboard.php
+++ app/Http/Livewire/Admin/Dashboard.php
@@ -4,16 +4,26 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 use Statamic\Facades\Entry;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class Dashboard extends Component
 {
     public $test;
     public $gift_items;
+    public $hosts;
 
     public function mount()
     {
         //$this->test = 'brannan';
         $this->gift_items = Entry::query()->where('collection','items')->where('status', 'published')->get();
+        $this->hosts = User::permission('edit host')
+            ->orderBy('name')
+            ->whereHas('campaigns', function( Builder $query){
+                $query->where('year', '=', date('Y'));
+            })
+            ->get();
     }
     public function render()
     {
