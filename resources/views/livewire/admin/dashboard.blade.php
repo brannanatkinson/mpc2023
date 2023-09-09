a0d3f7f77848743dee99602e059cc6c586530983
Brannan Atkinson <brannanatkinson@gmail.com>
2023-09-08 20:33:38 -0500

    dash sp

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -72,7 +72,7 @@
                         </div>
                         <div class="mb-8 text-sm">
                             @if( $item->item_sponsor )
-                                {{ $item->item_sponsor[0]->title }}
+                                {{ $item->item_sponsor->title }}
                             @endif
                         </div>
                     </div>
