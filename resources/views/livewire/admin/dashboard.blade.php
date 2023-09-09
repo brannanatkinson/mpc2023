a121431751cd327a0f238f60ee1e27f7e4ce5988
Brannan Atkinson <brannanatkinson@gmail.com>
2023-09-08 20:03:18 -0500

    dash

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -68,7 +68,7 @@
                             
                         </div>
                         <div class="mb-4 text-3xl">
-                            {{ App\Models\Item::where('statamic_id', $item->id)->sales()->count() }}
+                            {{ App\Models\Item::where('statamic_id', $item->id) }}
                         </div>
                         <div class="mb-8 text-sm">
                             
