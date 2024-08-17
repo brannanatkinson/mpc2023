c7ef99342e0d33ad52fd094726721d21a4c5008b
Brannan Atkinson <brannanatkinson@gmail.com>
2024-08-16 21:07:23 -0500

    Still working

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -69,7 +69,7 @@
                                 
                             </div>
                             <div class="mb-4 text-3xl">
-                            @php ray( App\Models\Item::where('statamic_id', $item->id)->first() ) @endphp
+                            @php ray( 'brannan' ) @endphp
                             
                             
                             </div>
