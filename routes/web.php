77ba38ccaff17d52a6c19635ab5564d538631d9f
Brannan Atkinson <brannanatkinson@gmail.com>
2024-09-11 17:29:05 -0500

    Just welcome in web.php

--- routes/web.php
+++ routes/web.php
@@ -24,30 +24,11 @@ use Illuminate\Support\Facades\Http;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::statamic('uri', 'view');
-
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', Dashboard::class)->middleware(['auth:sanctum', 'verified'])->name('dashboard');
-    Route::get('/admin/hosts', AllHosts::class)->middleware(['auth:sanctum', 'verified', 'can:admin'])->name('admin.hosts');
-    Route::get('/admin/hosts/passwordreset', AllHosts::class)->middleware(['auth:sanctum', 'verified', 'can:admin'])->name('admin.password');
-    Route::get('/update/host', UpdateHostForm::class)->middleware(['auth:sanctum', 'verified', 'can:edit host'])->name('admin.update.hosts');
-    // Routes to update statamic Categories, Items, and Sponsors
-    Route::get('/admin/statamic', [DbApi::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
 });
-Route::get('/hosts/{url}', HostPublicPage::class);
-Route::get('/mpc-board-dashboard', BoardDashboard::class);
 
-Route::post('/webhook', WebhookConfirmation::class);
-Route::get('/thankyou/{order_token}', OrderConfirmation::class);
+
 
 
 
