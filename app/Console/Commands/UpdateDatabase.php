9d1eca5c739e305ae1ffab4506c7c00da7656668
Brannan Atkinson <brannanatkinson@gmail.com>
2024-09-12 08:54:55 -0500

    Remove ray

--- app/Console/Commands/UpdateDatabase.php
+++ app/Console/Commands/UpdateDatabase.php
@@ -37,7 +37,6 @@ class UpdateDatabase extends Command
         */
         $response = Http::get(env('APP_URL').'/api/taxonomies/items/terms');
         $result = json_decode($response);
-        ray( $result );
         $terms = '';
         foreach( $result->data as $term){
             $category = Category::updateOrCreate(
