b9ac5026e25301dcb8f41dc4dec65cf05c8bdb83
Brannan Atkinson <brannanatkinson@gmail.com>
2024-09-23 11:21:09 -0500

    Fixed title in guest blade

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Housing Hope @php echo date('Y') @endphp - The Mary Parrish Center</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
