3359ff4ba150103af11c17ef906cfbf391fe3cc3
Brannan Atkinson <brannanatkinson@gmail.com>
2025-08-08 12:37:32 -0500

    Update sponsorship grid with new margins

--- resources/views/livewire/show-sponsor-grid.blade.php
+++ resources/views/livewire/show-sponsor-grid.blade.php
@@ -56,11 +56,13 @@
                     <div class="container max-w-3xl mx-auto grid mb-8">
                         <div class="flex justify-center">  
                             {{ collection:sponsors sponsor_type:contains="4" sort="title"}}
+                            <div>
                                 <a href="http://{{ sponsor_website }}" target="_blank" class="">
                                     {{ logo }}
-                                        <img src="{{ url }}" class="w-1/3 mx-auto" alt="">
+                                        <img src="{{ url }}" class="w-1/2 mx-auto" alt="">
                                     {{ /logo }}
                                 </a>
+                            <div>
                             {{ /collection:sponsors }}
                         </div>
                     </div>
@@ -72,15 +74,15 @@
             <div class="mb-4 text-center font-display text-3xl text-mp-blue-green">Matching Sponsors</div>
             <div class="container grid grid-cols-1 md:grid-cols-4 mb-8 gap-8 ">
                 {{ collection:sponsors sponsor_type:contains="5" sort="title"}}
-                    <div class=" h-64 w-full bg-white ">  
+                    <div class=" h-32 w-full bg-white ">  
                         <div class="flex justify-center h-full items-center mb-4">
                             {{ logo }} 
                                 {{ if sponsor_website }}   
                                 <a href="http://{{ sponsor_website }}">
-                                    <img src="{{ url }}" class="object-contain h-64 w-64" alt="">
+                                    <img src="{{ url }}" class="object-contain h-32 w-32" alt="">
                                 </a>
                                 {{ else }}
-                                    <img src="{{ url }}" class="object-contain h-64 w-64" alt="">
+                                    <img src="{{ url }}" class="object-contain h-32 w-32" alt="">
                                 {{ endif }}
                             {{ /logo }}
                         </div>
